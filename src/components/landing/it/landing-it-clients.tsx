01e938c1a7deb734d9d3150ed932805bf08d17eb
saile <eliasiv.codes@gmail.com>
2025-07-19 01:32:25 -0700

    Add responsiveness to landing clients section

--- src/components/landing/it/landing-it-clients.tsx
+++ src/components/landing/it/landing-it-clients.tsx
@@ -1,87 +1,94 @@
 import { Star } from "lucide-react";
 
+function LandingITClientsSecondaryTestimonial({}: {
+  // src: string;
+  // alt: string;
+  // testimonial: string;
+  // company: string;
+  // name: string;
+  // rating: number;
+}) {
+  return (
+    <div className="flex gap-4">
+      <div className="flex-1/3">
+        <div className="aspect-square">
+          <img
+            src={"/landing/landing-clients-1.jpg"}
+            className="rounded-3xl object-cover"
+            alt="Client 1"
+          />
+        </div>
+      </div>
+      <div className="flex-2/3 space-y-2 text-sm">
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur autem voluptas cumque quasi error fugit
+          eos dignissimos, delectus id facilis.
+        </p>
+        <div className="space-y-1">
+          <p>- Company, Name</p>
+          <div className="flex">
+            {Array.from([1, 2, 3, 4, 5]).map((ind) => (
+              <Star
+                key={ind}
+                className="fill-amber-300 stroke-amber-300"
+              />
+            ))}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+function LandingITClientsMainTestimonial({}: {
+  // src: string;
+  // alt: string;
+  // testimonial: string;
+  // company: string;
+  // name: string;
+  // rating: number;
+}) {
+  return (
+    <div className="space-y-5 sm:flex-1/2">
+      <div className="mx-auto">
+        <img
+          src={"/landing/landing-clients-1.jpg"}
+          className="mx-auto h-full w-full rounded-3xl object-cover"
+          alt="Main Client Testimonial"
+        />
+      </div>
+      <div className="space-y-2 text-sm">
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel maxime ad ullam maiores sequi ducimus accusantium
+          ipsa libero rem placeat, eveniet.
+        </p>
+        <div className="flex justify-between">
+          <p>- Company, Name</p>
+          <div className="flex">
+            {Array.from([1, 2, 3, 4, 5]).map((ind) => (
+              <Star
+                key={ind}
+                className="fill-amber-300 stroke-amber-300"
+              />
+            ))}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
 export default function LandingITClients() {
   return (
-    <section className="px-4">
-      <div className="mx-auto max-w-7xl min-w-[325px]">
-        <header className="mb-5 text-center text-lg font-bold sm:text-left sm:text-2xl">Most Recent Clients</header>
-        <div className="sm:flex sm:gap-8">
-          <div>
-            <img
-              src={"/landing/landing-clients-1.jpg"}
-              className="rounded-3xl object-cover"
-              alt="Client 1"
-            />
-            <div>
-              <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel maxime ad ullam maiores sequi ducimus
-                accusantium ipsa libero rem placeat, eveniet, eum necessitatibus sed iusto mollitia recusandae aperiam
-                adipisci. Sequi.
-              </p>
-              <div className="flex justify-between">
-                <p>- Company, Name</p>
-                <div className="flex">
-                  {Array.from([1, 2, 3, 4, 5]).map((ind) => (
-                    <Star
-                      key={ind}
-                      className="fill-amber-300 stroke-amber-300"
-                    />
-                  ))}
-                </div>
-              </div>
-            </div>
-          </div>
-          <div>
-            <div className="flex gap-4">
-              <img
-                src={"/landing/landing-clients-1.jpg"}
-                className="h-[150px] w-[150px] rounded-3xl object-cover"
-                alt="Client 1"
-              />
-              <div>
-                <p>
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur autem voluptas cumque quasi
-                  error fugit eos dignissimos, delectus id facilis, officia illo sequi tempore odio animi ab accusantium
-                  illum dolores!
-                </p>
-                <div>
-                  <p>- Company, Name</p>
-                  <div className="flex">
-                    {Array.from([1, 2, 3, 4, 5]).map((ind) => (
-                      <Star
-                        key={ind}
-                        className="fill-amber-300 stroke-amber-300"
-                      />
-                    ))}
-                  </div>
-                </div>
-              </div>
-            </div>
-            <div className="flex gap-4">
-              <img
-                src={"/landing/landing-clients-1.jpg"}
-                className="h-[150px] w-[150px] rounded-3xl object-cover"
-                alt="Client 1"
-              />
-              <div>
-                <p>
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur autem voluptas cumque quasi
-                  error fugit eos dignissimos, delectus id facilis, officia illo sequi tempore odio animi ab accusantium
-                  illum dolores!
-                </p>
-                <div>
-                  <p>- Company, Name</p>
-                  <div className="flex">
-                    {Array.from([1, 2, 3, 4, 5]).map((ind) => (
-                      <Star
-                        key={ind}
-                        className="fill-amber-300 stroke-amber-300"
-                      />
-                    ))}
-                  </div>
-                </div>
-              </div>
-            </div>
+    <section>
+      <div className="text-pwr-primary mx-auto max-w-6xl min-w-[325px] px-5">
+        <header className="mb-5 text-center text-2xl font-bold sm:text-left sm:text-3xl">Most Recent Clients</header>
+        <div className="space-y-10 sm:flex sm:gap-10">
+          <LandingITClientsMainTestimonial />
+          <div className="flex flex-col gap-5 sm:flex-1/2">
+            <LandingITClientsSecondaryTestimonial />
+            <LandingITClientsSecondaryTestimonial />
+            <LandingITClientsSecondaryTestimonial />
           </div>
         </div>
       </div>
